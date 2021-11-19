0ff0d81444de4c11fe5029173dff5ae4348de5da
cxdcox <daryl.cox@checkmarx.com>
2021-11-19 15:11:49 -0600

    Bat and YAML updates - 15

--- ADO_CxFlow_CommandLineScript_1.bat
+++ ADO_CxFlow_CommandLineScript_1.bat
@@ -1,6 +1,6 @@
 rem ADO: Run this as a 'Command Line Script' Task version 2.*
 rem This will download the CxFlow JAR into the 'temp' directory (which should be $(Agent.TempDirectory)) and unzip it...
-rem Version: v1.0114
+rem Version: v1.0115
 
 rem - - - - - Set various CxCLI Variables - - - - -     
 
@@ -9,8 +9,8 @@ set CX_USER=dcox
 set CX_PSWD=C0rky9#2016
 
 set CX_TEAM=/CxServer/SP/Company/Users
-set CX_PROJECT=ADO-14-CxFlow-MyFirstProject
-set CX_APP=ADO-14
+set CX_PROJECT=ADO-15-CxFlow-MyFirstProject
+set CX_APP=ADO-15
 
 rem set CX_ADO_OWNER=cxdcox, CX_ADO_REPO=MyFirstProject , CX_ADO_BRANCH=master
 
@@ -45,7 +45,7 @@ pushd .
 cd $(Agent.TempDirectory)
 
 echo "Using 'curl' to download the CxFlow JAR file..."
-curl --output cx-flow-1.6.22.jar -L "https://github.com/checkmarx-ltd/cx-flow/releases/download/1.6.22/cx-flow-1.6.22.jar"
+curl --output cx-flow-1.6.26.jar -L "https://github.com/checkmarx-ltd/cx-flow/releases/download/1.6.26/cx-flow-1.6.26.jar"
 echo "Using 'curl' to download the CxFlow YAML file..."
 curl --output application-azure_v9.4.0.yml "https://raw.githubusercontent.com/cxdcox/ADO-CxFlow-Control/main/application-azure_v9.4.0.yml"
 echo "Directory of . (should be $(Agent.TempDirectory)) after 'curl' download:"
@@ -53,8 +53,8 @@ dir .
 echo "Display of the downloaded 'application-azure_v9.4.0.yml' file..."
 type application-azure_v9.4.0.yml
 
-echo "Invoking: java -jar cx-flow-1.6.22.jar --scan --f="$(Build.Repository.LocalPath)" --spring.config.location="application-azure_v9.4.0.yml" --cx-team="%CX_TEAM%" --cx-project="%CX_PROJECT%" --app="%CX_APP%" --namespace="cx-drc" --repo-name="$(Build.Repository.Name)" --branch="$(Build.SourceBranchName)" "
-java -jar cx-flow-1.6.22.jar --scan --f="$(Build.Repository.LocalPath)" --spring.config.location="application-azure_v9.4.0.yml" --cx-team="%CX_TEAM%" --cx-project="%CX_PROJECT%" --app="%CX_APP%" --namespace="cx-drc" --repo-name="$(Build.Repository.Name)" --branch="$(Build.SourceBranchName)"
+echo "Invoking: java -jar cx-flow-1.6.26.jar --scan --f="$(Build.Repository.LocalPath)" --spring.config.location="application-azure_v9.4.0.yml" --cx-team="%CX_TEAM%" --cx-project="%CX_PROJECT%" --app="%CX_APP%" --namespace="cx-drc" --repo-name="$(Build.Repository.Name)" --branch="$(Build.SourceBranchName)" "
+java -jar cx-flow-1.6.26.jar --scan --f="$(Build.Repository.LocalPath)" --spring.config.location="application-azure_v9.4.0.yml" --cx-team="%CX_TEAM%" --cx-project="%CX_PROJECT%" --app="%CX_APP%" --namespace="cx-drc" --repo-name="$(Build.Repository.Name)" --branch="$(Build.SourceBranchName)"
 
 echo "Switching back to the 'source' directory..."
 popd
